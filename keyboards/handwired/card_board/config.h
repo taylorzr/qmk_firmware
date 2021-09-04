984b48cf3e5f0ca6e47b35a9931bd65f4f6218c4
Zach Taylor <zach@taylorzr.com>
2021-09-04 19:45:02 -0400

    Card Board WIP

---
+++ keyboards/handwired/card_board/config.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID 0xFEED
+#define PRODUCT_ID 0x0000
+#define DEVICE_VER 0x0001
+#define MANUFACTURER taylorzr
+#define PRODUCT card_board
+
+/* key matrix size */
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 12
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+ */
+#define MATRIX_ROW_PINS \
+    { D1, D0, D4, C6 }
+#define MATRIX_COL_PINS \
+    { D7, E6, B4, B5, B6, B2, B3, B1, F7, F6, F5, F4 }
+
+#define DIODE_DIRECTION COL2ROW
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* define if matrix has ghost (lacks anti-ghosting diodes) */
+//#define MATRIX_HAS_GHOST
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+
+/**
+ * Aranck-specific definitions
+ */
+#define AUDIO_PIN B5
+/**
+ * Aranck-specific definitions END
+ */
+
+
+#define TAPPING_TERM 200
---
+++ keyboards/handwired/card_board/keymaps/default/keymap.c
@@ -0,0 +1,49 @@
+/* Copyright 2021 Zach Taylor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+enum card_board_layers {
+  _QWERTY,
+  _LOWER,
+  _RAISE
+};
+
+#define RAISE MO(_RAISE)
+#define LOWER MO(_RAISE)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_QWERTY] = LAYOUT_default(
+     KC_EQL,         KC_Q,   KC_W,   KC_E,   KC_R,           KC_T,            KC_Y,              KC_U,              KC_I,    KC_O,   KC_P,    KC_MINS,
+     LCTL_T(KC_TAB), KC_A,   KC_S,   KC_D,   KC_F,           KC_G,            KC_H,              KC_J,              KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+     LSFT_T(KC_GRV), KC_Z,   KC_X,   KC_C,   KC_V,           KC_B,            KC_N,              KC_M,              KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_BSLASH),
+     /*___*/         /*___*/ /*___*/ KC_DEL, LALT_T(KC_ESC), LGUI_T(KC_BSPC), LT(_LOWER,KC_SPC), LT(_RAISE,KC_ENT), KC_BSLASH
+  ),
+
+    [_LOWER] = LAYOUT_default(
+     _______, KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______,
+     _______, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5), LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), _______,
+     _______, _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    KC_BSLASH,  _______,
+     /*___*/  /*___*/     /*___*/     _______,    _______,    KC_BSPC,    _______,    _______,    _______
+  ),
+
+
+  [_RAISE] = LAYOUT_default(
+     _______, _______, _______, LSFT(KC_LBRC), LSFT(KC_RBRC), _______, KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT,  _______, _______,
+     _______, _______, _______, KC_LBRC,       KC_RBRC,       _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,
+     _______, _______, _______, LSFT(KC_9),    LSFT(KC_0),    _______, _______, _______, _______, _______,  _______, _______,
+     /*___*/  /*___*/  /*___*/  _______,       _______,       _______, _______, _______, _______
+  ),
+};
