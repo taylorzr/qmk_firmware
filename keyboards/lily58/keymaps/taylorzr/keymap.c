016c607e399e71b0813bb3f5b96a643d6b423767
Zach Taylor <zach@taylorzr.com>
2021-09-09 09:14:20 -0400

    Update lily to match other keymaps

--- keyboards/lily58/keymaps/taylorzr/keymap.c
+++ keyboards/lily58/keymaps/taylorzr/keymap.c
@@ -3,69 +3,42 @@
 enum my_layers {
   _QWERTY = 0,
   _COLEMAK,
-  _KYRIA,
+  _LOWER,
   _RAISE,
-  _ADJUST,
-};
-
-enum my_keycodes {
-  BRACKETS = SAFE_RANGE,
-  BRACES,
-  PARENS,
-  TILDES,
-};
-
-enum my_tapdances {
-  TD_LBRC_HOME = 0,
-  TD_RBRC_END,
-};
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-  [TD_LBRC_HOME] = ACTION_TAP_DANCE_DOUBLE(KC_LBRC, KC_HOME),
-  [TD_RBRC_END]  = ACTION_TAP_DANCE_DOUBLE(KC_RBRC, KC_END),
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
-    KC_EQL,          KC_1,   KC_2,   KC_3,    KC_4,     KC_5,           /*___*/           /*******/ /*___*/          KC_6,              KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,         \
-    LALT_T(KC_TAB),  KC_Q,   KC_W,   KC_E,    KC_R,     KC_T,           /*___*/           /*******/ /*___*/          KC_Y,              KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLASH,       \
-    LCTL_T(KC_GRV),  KC_A,   KC_S,   KC_D,    KC_F,     KC_G,           /*___*/           /*******/ /*___*/          KC_H,              KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,         \
-    LSFT_T(KC_LBRC), KC_Z,   KC_X,   KC_C,    KC_V,     KC_B,           TD(TD_LBRC_HOME), /*******/ TD(TD_RBRC_END), KC_N,              KC_M,    KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_RBRC), \
-    /*___*/          /*___*/ /*___*/ KC_LEFT, KC_RIGHT, LGUI_T(KC_ESC), KC_BSPC,          /*******/ KC_SPC,          LT(_RAISE,KC_ENT), KC_DOWN, KC_UP    \
+    KC_GRV,         KC_1,   KC_2,   KC_3,    KC_4,     KC_5,           /*___*/          /*******/ /*___*/            KC_6,              KC_7,    KC_8,    KC_9,   KC_0,    KC_BSLASH,         \
+    KC_EQL,         KC_Q,   KC_W,   KC_E,    KC_R,     KC_T,           /*___*/          /*******/ /*___*/            KC_Y,              KC_U,    KC_I,    KC_O,   KC_P,    KC_MINS,           \
+    LCTL_T(KC_TAB), KC_A,   KC_S,   KC_D,    KC_F,     KC_G,           /*___*/          /*******/ /*___*/            KC_H,              KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,           \
+    LSFT_T(KC_GRV), KC_Z,   KC_X,   KC_C,    KC_V,     KC_B,           KC_HOME,         /*******/ KC_END,            KC_N,              KC_M,    KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_BSLASH), \
+    /*___*/         /*___*/ /*___*/ KC_LEFT, KC_RIGHT, LALT_T(KC_ESC), LGUI_T(KC_BSPC), /*******/ LT(_LOWER,KC_SPC), LT(_RAISE,KC_ENT), KC_DOWN, KC_UP    \
   ),
 
   [_COLEMAK] = LAYOUT( \
-    KC_EQL,          KC_1,   KC_2,   KC_3,    KC_4,     KC_5,           /*___*/           /*******/ /*___*/          KC_6,              KC_7,    KC_8,    KC_9,   KC_0,    KC_MINS,         \
-    LALT_T(KC_TAB),  KC_Q,   KC_W,   KC_F,    KC_P,     KC_B,           /*___*/           /*******/ /*___*/          KC_J,              KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSLASH,       \
-    LCTL_T(KC_GRV),  KC_A,   KC_R,   KC_S,    KC_T,     KC_G,           /*___*/           /*******/ /*___*/          KC_M,              KC_N,    KC_E,    KC_I,   KC_O,    KC_QUOT,         \
-    LSFT_T(KC_LBRC), KC_Z,   KC_X,   KC_C,    KC_D,     KC_V,           TD(TD_LBRC_HOME), /*******/ TD(TD_RBRC_END), KC_K,              KC_H,    KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_RBRC), \
-    /*___*/          /*___*/ /*___*/ KC_LEFT, KC_RIGHT, LGUI_T(KC_ESC), KC_BSPC,          /*******/ KC_SPC,          LT(_RAISE,KC_ENT), KC_DOWN, KC_UP    \
+    _______, _______, _______, _______, _______, _______, /*___*/  /*******/ /*___*/  _______, _______, _______, _______, _______, _______, \
+    _______, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    /*___*/  /*******/ /*___*/  KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, _______, \
+    _______, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    /*___*/  /*******/ /*___*/  KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    _______, \
+    _______, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    _______, /*******/ _______, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, _______, \
+    /*___*/  /*___*/  /*___*/  _______, _______, _______, _______, /*******/ _______, _______, _______, _______  \
   ),
 
-  [_KYRIA] = LAYOUT( \
-    KC_EQL,          KC_NO,  KC_NO,  KC_NO,   KC_NO,    KC_NO,          /*___*/  /*******/  /*___*/ KC_NO,             KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_MINS,         \
-    LALT_T(KC_TAB),  KC_Q,   KC_W,   KC_E,    KC_R,     KC_T,           /*___*/  /*******/  /*___*/ KC_Y,              KC_U,    KC_I,    KC_O,   KC_P,    KC_BSLASH,       \
-    LCTL_T(KC_GRV),  KC_A,   KC_S,   KC_D,    KC_F,     KC_G,           /*___*/  /*******/  /*___*/ KC_H,              KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,         \
-    LSFT_T(KC_LBRC), KC_Z,   KC_X,   KC_C,    KC_V,     KC_B,           KC_NO,   /*******/  KC_NO,  KC_N,              KC_M,    KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_RBRC), \
-    /*___*/          /*___*/ /*___*/ KC_LEFT, KC_RIGHT, LGUI_T(KC_ESC), KC_BSPC, /*******/  KC_SPC, LT(_RAISE,KC_ENT), KC_DOWN, KC_UP    \
+  [_LOWER] = LAYOUT( \
+    _______, DF(_QWERTY), DF(_COLEMAK), _______,    _______,    _______,    /*___*/  /*******/ /*___*/  _______,    _______,    _______,    _______,    _______,    _______, \
+    _______, KC_1,        KC_2,         KC_3,       KC_4,       KC_5,       /*___*/  /*******/ /*___*/  KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______, \
+    _______, LSFT(KC_1),  LSFT(KC_2),   LSFT(KC_3), LSFT(KC_4), LSFT(KC_5), /*___*/  /*******/ /*___*/  LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), _______, \
+    _______, _______,     _______,      _______,    _______,    _______,    _______, /*******/ _______, _______,    _______,    _______,    _______,    _______,    _______, \
+    /*___*/  /*___*/      /*___*/       _______,    _______,    _______,    KC_BSPC, /*******/ _______, _______,    _______,    _______     \
   ),
 
   [_RAISE] = LAYOUT( \
-    _______,  DF(_QWERTY), DF(_COLEMAK), DF(_KYRIA), KC_MPLY,    KC_MNXT,    /*___*/  /*******/ /*___*/  KC_VOLD,    KC_VOLU,    _______,    _______,    _______,    _______, \
-    _______,  KC_1,        KC_2,         KC_3,       KC_4,       KC_5,       /*___*/  /*******/ /*___*/  KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       _______, \
-    BRACES,   LSFT(KC_1),  LSFT(KC_2),   LSFT(KC_3), LSFT(KC_4), LSFT(KC_5), /*___*/  /*******/ /*___*/  LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), PARENS,  \
-    BRACKETS, _______,     _______,      _______,    KC_LEFT,    KC_RIGHT,   KC_HOME, /*******/ KC_END,  KC_DOWN,    KC_UP,      _______,    _______,    _______,    _______, \
-     /*___*/  /*___*/      /*___*/       _______,    _______,    KC_DEL,     _______, /*******/ _______, _______,    KC_SLEP,    _______     \
+    _______, _______, _______, _______,       _______,       _______, /*___*/  /*******/ /*___*/  _______, _______, _______, _______,  _______, _______, \
+    _______, _______, _______, LSFT(KC_LBRC), LSFT(KC_RBRC), _______, /*___*/  /*******/ /*___*/  KC_MPLY, KC_VOLD, KC_VOLU, KC_MNXT,  _______, KC_DEL,  \
+    _______, _______, _______, KC_LBRC,       KC_RBRC,       _______, /*___*/  /*******/ /*___*/  KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, \
+    _______, _______, _______, LSFT(KC_9),    LSFT(KC_0),    _______, _______, /*******/ _______, _______, _______, _______, _______,  _______, _______, \
+    /*___*/  /*___*/  /*___*/  _______,       _______,       _______, _______, /*******/ _______, _______, _______, _______  \
   ),
-
-  [_ADJUST] = LAYOUT( \
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-                               _______, _______, _______, _______, _______,  _______, _______, _______ \
-  ),
-
 };
 
 /* layer_state_t layer_state_set_user(layer_state_t state) { */
@@ -107,40 +80,3 @@ void oled_task_user(void) {
   }
 }
 #endif // OLED_DRIVER_ENABLE
-
-// TODO: Do I need this?!?! Don't think so?
-/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
-/*   if (record->event.pressed) { */
-/* #ifdef OLED_DRIVER_ENABLE */
-/*     set_keylog(keycode, record); */
-/* #endif */
-/*     // set_timelog(); */
-/*   } */
-/*   return true; */
-/* } */
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case BRACKETS:
-      if (record->event.pressed) {
-        SEND_STRING("[]"SS_TAP(X_LEFT));
-      }
-      break;
-    case BRACES:
-      if (record->event.pressed) {
-        SEND_STRING("{}"SS_TAP(X_LEFT));
-      }
-      break;
-    case PARENS:
-      if (record->event.pressed) {
-        SEND_STRING("()"SS_TAP(X_LEFT));
-      }
-      break;
-    case TILDES:
-      if (record->event.pressed) {
-        SEND_STRING("``"SS_TAP(X_LEFT));
-      }
-      break;
-  }
-  return true;
-};
