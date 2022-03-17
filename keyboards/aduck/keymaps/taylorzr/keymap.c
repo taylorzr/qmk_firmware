d81498c778a8f069267842874239d478efadd740
Zach Taylor <zach@taylorzr.com>
2022-03-17 09:58:02 -0400

    Mostly combos

--- keyboards/aduck/keymaps/taylorzr/keymap.c
+++ keyboards/aduck/keymaps/taylorzr/keymap.c
@@ -1,5 +1,8 @@
 #include QMK_KEYBOARD_H
 
+#define SS_COPY S(G(C(KC_4)))
+#define SS_FILE S(G(KC_4))
+
 enum my_layers {
 	_COLEMAKDH = 0,
 	_QWERTY,
@@ -9,16 +12,8 @@ enum my_layers {
 	_NUM,
 };
 
-#define SS_COPY S(G(C(KC_4)))
-#define SS_FILE S(G(KC_4))
-
-enum custom_keycodes {
-  KC_SPC_ENT = SAFE_RANGE,
-};
-
 enum {
-	X_DEL = 0,
-	SCLN,
+	SCLN = 0,
 	CLN,
 	SINGLE_QUOT,
 	DOUBLE_QUOT,
@@ -29,12 +24,30 @@ enum {
 	LESS,
 };
 
+
+// dont forget to update combo count in config.h
+const uint16_t PROGMEM fp_combo[] = {KC_F, KC_P, COMBO_END};
+const uint16_t PROGMEM cd_combo[] = {KC_C, KC_D, COMBO_END};
+const uint16_t PROGMEM dv_combo[] = {KC_D, KC_V, COMBO_END};
+const uint16_t PROGMEM lu_combo[] = {KC_L, KC_U, COMBO_END};
+const uint16_t PROGMEM kh_combo[] = {KC_K, KC_H, COMBO_END};
+const uint16_t PROGMEM hcomma_combo[] = {KC_H, KC_COMM, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(fp_combo, KC_DEL),
+    COMBO(cd_combo, KC_TAB),
+    COMBO(dv_combo, S(KC_EQL)),
+    COMBO(lu_combo, S(KC_GRV)),
+    COMBO(kh_combo, KC_EQL),
+    COMBO(hcomma_combo, KC_ENT),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_COLEMAKDH] = LAYOUT( \
-			KC_Q,        KC_W,        KC_F,        KC_P,          KC_B,            /*******/ KC_J,             KC_L,            KC_U,    KC_Y,        KC_MINS, \
-			ALT_T(KC_A), GUI_T(KC_R), SFT_T(KC_S), KC_T,          KC_G,            /*******/ KC_M,             KC_N,            KC_E,    GUI_T(KC_I), KC_O,    \
-			KC_Z,        TD(X_DEL),   KC_C,        KC_D,          KC_V,            /*******/ KC_K,             KC_H,            KC_COMM, KC_DOT,      KC_SLSH, \
-			/*___*/      /*___*/      /*___*/      CTL_T(KC_ESC), LT(_SFT,KC_SPC), /*******/ LT(_SYM,KC_BSPC), LT(_NAV,KC_SCLN) \
+			KC_Q,        KC_W,        KC_F,        KC_P,          KC_B,             /*******/ KC_J,            KC_L,            KC_U,    KC_Y,   KC_MINS, \
+			ALT_T(KC_A), GUI_T(KC_R), GUI_T(KC_S), SFT_T(KC_T),   KC_G,             /*******/ KC_M,            SFT_T(KC_N),     KC_E,    KC_I,   KC_O,    \
+			KC_Z,        KC_X,        KC_C,        KC_D,          KC_V,             /*******/ KC_K,            KC_H,            KC_COMM, KC_DOT, KC_SLSH, \
+			/*___*/      /*___*/      /*___*/      CTL_T(KC_ESC), LT(_SFT,KC_BSPC), /*******/ LT(_SYM,KC_SPC), LT(_NAV,KC_SCLN) \
 			),
 
 	[_QWERTY] = LAYOUT( \
@@ -44,18 +57,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			/*___*/      /*___*/      /*___*/ CTL_T(KC_ESC), SFT_T(KC_SPC), /*******/ LT(_SYM,KC_BSPC), LT(_NAV,KC_MINS) \
 			),
 
+	// https://thomasbaart.nl/2018/12/09/qmk-basics-tap-and-hold-actions/#a-workaround-for-mod-tap
+	// add mod taps to this layer see article
 	[_SFT] = LAYOUT( \
 			S(KC_Q), S(KC_W), S(KC_F), S(KC_P), S(KC_B), /*******/ S(KC_J), S(KC_L), S(KC_U),         S(KC_Y),         S(KC_MINS), \
 			S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_G), /*******/ S(KC_M), S(KC_N), S(KC_E),         S(KC_I),         S(KC_O),    \
 			S(KC_Z), S(KC_X), S(KC_C), S(KC_D), S(KC_V), /*******/ S(KC_K), S(KC_H), TD(DOUBLE_QUOT), TD(SINGLE_QUOT), S(KC_SLSH), \
-			/*___*/  /*___*/  /*___*/  _______, _______, /*******/ KC_ENT,  TD(CLN)  \
+			/*___*/  /*___*/  /*___*/  _______, _______, /*******/ _______, TD(CLN)  \
 			),
 
 	[_SYM] = LAYOUT( \
 			KC_1,     KC_2,     KC_3,       KC_4,       KC_5,      /*******/ KC_6,    KC_7,        KC_8,    KC_9,         KC_0,      \
 			S(KC_1),  S(KC_2),  S(KC_3),    S(KC_4),    S(KC_5),   /*******/ S(KC_6), S(KC_7),     S(KC_8), S(KC_BSLASH), KC_BSLASH, \
 			TD(LBRC), TD(RBRC), TD(LCURLY), S(KC_RBRC), S(KC_EQL), /*******/ KC_EQL,  TD(LPARENS), S(KC_0), TD(LESS),     S(KC_DOT), \
-			/*___*/   /*___*/   /*___*/     KC_GRV,     KC_ENT,    /*******/ _______, S(KC_GRV)    \
+			/*___*/   /*___*/   /*___*/     KC_GRV,     _______,   /*******/ _______, S(KC_GRV)    \
 			),
 
 	[_NAV] = LAYOUT( \
@@ -72,28 +87,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			/*___*/      /*___*/         /*___*/  TG(_NUM), _______,  /*******/ _______,        KC_0  \
 			),
 };
-
-uint16_t custom_lt_timer;
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	if(keycode == KC_SPC_ENT) {
-		if(record->event.pressed) {
-			custom_lt_timer = timer_read();
-			layer_on(_SYM);
-		}else{
-			layer_off(_SYM);
-			if (timer_elapsed(custom_lt_timer)<200) {
-        if (get_mods() & MOD_BIT(KC_LSHIFT) || get_mods() & MOD_BIT(KC_RSHIFT)){
-          register_code(KC_ENT);
-        } else {
-          register_code(KC_SPC);
-        }
-        unregister_code(KC_ENT);
-        unregister_code(KC_SPC);
-			}
-		}
-	}
-	return true;
-}
 
 typedef struct {
 	uint16_t single;
@@ -230,13 +223,15 @@ void cln_equals(qk_tap_dance_state_t *state, void *user_data) {
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
 		case GUI_T(KC_R):
-			return 250;
-		case SFT_T(KC_S):
-			return 250;
+			return TAPPING_TERM + 50;
+		case GUI_T(KC_S):
+			return TAPPING_TERM + 50;
+		case SFT_T(KC_T):
+			return TAPPING_TERM + 50;
+		case SFT_T(KC_N):
+			return TAPPING_TERM + 50;
 		case ALT_T(KC_A):
-			return 250;
-		case GUI_T(KC_I):
-			return 250;
+			return TAPPING_TERM + 50;
 		default:
 			return TAPPING_TERM;
 	}
@@ -246,11 +241,13 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
 	switch (keycode) {
 		case GUI_T(KC_R):
 			return true;
-		case SFT_T(KC_S):
+		case GUI_T(KC_S):
+			return true;
+		case SFT_T(KC_T):
+			return true;
+		case SFT_T(KC_N):
 			return true;
 		case ALT_T(KC_A):
-			return true;
-		case GUI_T(KC_I):
 			return true;
 		default:
 			return false;
@@ -258,7 +255,6 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
-  [X_DEL] = ACTION_TAP_DANCE_DOUBLE(KC_X, KC_DEL),
 	[CLN] = ACTION_TAP_DANCE_FN(cln_equals),
 	[SCLN] = ACTION_TAP_DANCE_FN(scln_equals),
 	[DOUBLE_QUOT] = ACTION_TAP_DANCE_FN(double_your_pleasure),
--- keyboards/aduck/keymaps/taylorzr/rules.mk
+++ keyboards/aduck/keymaps/taylorzr/rules.mk
@@ -17,6 +17,7 @@ RGBLIGHT_ENABLE    = no  # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE  = no  # Enable one-hand typing
 OLED_DRIVER_ENABLE = no  # OLED display
 TAP_DANCE_ENABLE   = yes
+COMBO_ENABLE       = yes
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
